f75cd6b2f1c93a14416e9663bd346b5bc6bc8c63
commodoremiller <commodoremiller@users.noreply.github.com>
2022-04-07 21:55:03 -0400

    Update to Player Movement
    Moving left and right w/ flipping animations.

---
+++ PlayerMovement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    //necessary for animations and physics
+    private Rigidbody2D rb2d;
+    private Animator myAnimator;
+    private bool facingRight = true;
+
+    // variables to play with
+    public float speed = 2.0f;
+    public float horizMovement;// = 1 [or] -1 [or] 0
+
+
+    private void Start()
+    {
+        // define the gameobjects found on player
+        rb2d = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // handles the input for physics
+    private void Update()
+    {
+        // check direction given by player
+        horizMovement = Input.GetAxis("Horizontal");
+    }
+    // handles running the physics
+    private void FixedUpdate()
+    {
+        //move the character left and right
+        rb2d.velocity = new Vector2(horizMovement * speed, rb2d.velocity.y);
+        Flip(horizMovement);
+        myAnimator.SetFloat("speed", Mathf.Abs(horizMovement));
+    }
+    //flipping function
+    private void Flip (float horizontal)
+    {
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+
+        }
+
+
+
+
+    }
+
+}
